146570ca14114a19ae145ce9da2f71c4b833024d
arul9976 <arunmicheal8@gmail.com>
2025-02-14 14:13:28 +0530

    Auth v1.0

--- CodeFusionUI/src/App.jsx
+++ CodeFusionUI/src/App.jsx
@@ -11,22 +11,15 @@ import EditorACE from "./Editor/EditorACE";
 
 const App = () => {
   return (
-   <Routes>  
-        <Route path="/loginRegister" element={<LoginRegister />} />
-        <Route path="/forgotPassword" element={<ForgotPassword />} />
-        <Route path="/resetPassword" element={<ResetPassword />} />
+
+    <Routes>
+      <Route path="/" element={<h1>Welcome</h1>} />
+      <Route path="/loginRegister" Component={LoginRegister} />
+      <Route path="/forgotPassword" Component={ForgotPassword} />
+      <Route path="/resetPassword" Component={ResetPassword} />
+      <Route path="/IDE" Component={EditorACE} />
+
     </Routes>
-
-    // <Routes>
-    //   <Route path="/" element={<h1>Welcome</h1>} />
-    //   <Route path="/loginRegister" Component={LoginRegister} />
-    //   {/* <Route path="/codeEditor" Component ={<CodeEditor/>} /> */}
-    //   <Route path="/forgotPassword" Component={ForgotPassword} />
-    //   <Route path="/resetPassword" Component={ResetPassword} />
-    //   {/* <Route path="/chat" Component={Chat} /> */}
-    //   <Route path="/IDE" Component={CodeEditor} />
-
-    // </Routes>
 
   )
 };
